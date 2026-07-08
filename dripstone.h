68e41effb4db78e0c48d048d21d9bc3c0217cbb9
RIHGS <grhigsam@gmail.com>
2026-07-08 23:38:57 +0545

    added h file for ds

---
+++ dripstone.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+
+class Dripstone
+{
+public:
+    sf::ConvexShape shape;
+    sf::Vector2f velocity;
+};
+
+class DripstoneManager
+{
+private:
+    std::vector<Dripstone> dripstones;
+
+    float spawnTimer = 0.f;
+    float spawnInterval = 1.2f;
+    float fallSpeed = 400.f;
+
+public:
+
+    void update(float dt)
+    {
+        spawnTimer += dt;
+
+        if (spawnTimer >= spawnInterval)
+        {
+            spawnTimer = 0.f;
+
+            Dripstone d;
+
+            d.shape.setPointCount(3);
+
+            d.shape.setPoint(0, { 0.f,0.f });
+            d.shape.setPoint(1, { 40.f,0.f });
+            d.shape.setPoint(2, { 20.f,70.f });
+
+            d.shape.setFillColor(sf::Color(90, 90, 90));
+
+            float x = static_cast<float>(rand() % 760);
+
+            d.shape.setPosition({ x,-70.f });
+
+            d.velocity = { 0.f, fallSpeed };
+
+            dripstones.push_back(d);
+        }
+
+        for (auto& d : dripstones)
+        {
+            d.shape.move(d.velocity * dt);
+        }
+
+        dripstones.erase(
+            std::remove_if(
+                dripstones.begin(),
+                dripstones.end(),
+                [](const Dripstone& d)
+                {
+                    return d.shape.getPosition().y > 700;
+                }),
+            dripstones.end());
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        for (auto& d : dripstones)
+            window.draw(d.shape);
+    }
+
+    bool checkCollision(const sf::FloatRect& playerBounds)
+    {
+        for (auto& d : dripstones)
+        {
+            if (playerBounds.findIntersection(d.shape.getGlobalBounds()))
+                return true;
+        }
+
+        return false;
+    }
+
+    void reset()
+    {
+        dripstones.clear();
+        spawnTimer = 0.f;
+    }
+};
